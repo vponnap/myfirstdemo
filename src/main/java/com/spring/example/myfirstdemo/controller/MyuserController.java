243ade3de72976b80a4e7e045bf2b1160e2aa14c
Venkata Ponnapalli <venkataponnapalli@MacBook-Pro.local>
2020-07-05 17:49:32 +0530

    my first commit

---
+++ src/main/java/com/spring/example/myfirstdemo/controller/MyuserController.java
@@ -0,0 +1,16 @@
+package com.spring.example.myfirstdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyuserController {
+
+    @GetMapping("/hellovenka")
+    public String getNames() {
+        return ( "Welcome Venka to Dockers and K8S");
+    }
+
+
+}
---
+++ src/main/java/com/spring/example/myfirstdemo/controller/Pratice.java
@@ -0,0 +1,8 @@
+package com.spring.example.myfirstdemo.controller;
+
+public class Pratice {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Practice Class");
+    }
+}
