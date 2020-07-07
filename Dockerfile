3da881469202329f62158b857f2499a5ec8abea8
Venkata Ponnapalli <venkataponnapalli@MacBook-Pro.domain.name>
2020-07-07 18:10:20 +0530

    this is committed for port

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
-EXPOSE 8090
+EXPOSE 9090
 ADD build/libs/myfirstdemo-0.0.1-SNAPSHOT.jar myfirstdemo-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java", "-jar", "/myfirstdemo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/spring/example/myfirstdemo/controller/Pratice.java
+++
@@ -1,8 +0,0 @@
-package com.spring.example.myfirstdemo.controller;
-
-public class Pratice {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Practice Class");
-    }
-}
